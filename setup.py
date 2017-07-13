93f9b9a4373689d1068560a71b27892455db9e7d
Tony Duckles <tony@nynim.org>
2017-07-12 20:55:03 -0500

    Version 0.5

--- setup.py
+++ setup.py
@@ -2,7 +2,11 @@ from setuptools import setup
 
 setup(
     name='linkpad',
-    version='0.1',
+    version='0.5',
+    description='A command-line bookmark manager',
+    author='Tony Duckles',
+    author_email='tony@nynim.org',
+    url='https://github.com/tonyduckles/linkpad',
     py_modules=['linkpad'],
     install_requires=[
         'click',
@@ -10,6 +14,7 @@ setup(
         'sh',
         'bs4',
     ],
+    license='MIT',
     entry_points="""
         [console_scripts]
         linkpad=linkpad:cli
